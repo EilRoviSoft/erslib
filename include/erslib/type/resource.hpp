7c037288af37903d62179cb12aeac69ff8e0471f
EilRoviSoft <el.rovisoft@gmail.com>
2026-03-05 01:34:01 +0300

    Fixed type/resource

--- include/erslib/type/resource.hpp
+++ include/erslib/type/resource.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 // std
+#include <atomic>
 #include <memory>
 
 // boost
@@ -8,16 +9,27 @@
 #include <boost/thread/shared_mutex.hpp>
 
 // ers
+#include <erslib/assert.hpp>
 #include <erslib/type/result.hpp>
 
+
+// Forward declaration
+
+namespace ers {
+    template<typename T>
+    class Accessor;
+}
+
+
+// Resource
 
 namespace ers {
     template<typename T>
     class IResource {
+        friend class Accessor<T>;
+
+
     public:
-        using storage_type = std::shared_ptr<T>;
-
-
         // Destructor
 
         virtual ~IResource() = default;
@@ -25,7 +37,7 @@ namespace ers {
 
         // Observers
 
-        Result<storage_type> get() const {
+        Result<Accessor<T>> get() const {
             boost::upgrade_lock r_lock(_mutex);
 
             if (!m_value) {
@@ -37,43 +49,23 @@ namespace ers {
                 }
             }
 
-            return m_value;
+            return Accessor<T>(*this);
         }
 
 
     protected:
-        mutable storage_type m_value = nullptr;
+        mutable std::unique_ptr<T> m_value;
+        mutable std::atomic<size_t> m_refs = 0;
 
 
         virtual Status load() const = 0;
-        virtual Status unload() const {
+        virtual void unload() const {
             m_value.reset();
-            return ok;
         }
 
 
     private:
         mutable boost::upgrade_mutex _mutex;
-
-
-        storage_type _make_external_ptr() {
-            auto self = this;
-
-            return storage_type(
-                m_value.get(),
-                [self, inner = m_value](T*) mutable {
-                    inner.reset();
-
-                    boost::unique_lock r_lock(self->_mutex);
-
-                    if (self->m_value && self->m_value.use_count() == 1) {
-                        boost::upgrade_to_unique_lock w_lock(r_lock);
-
-                        self->unload();
-                    }
-                }
-            );
-        }
     };
 
 
@@ -87,5 +79,100 @@ namespace ers {
 
     protected:
         std::string m_path;
+    };
+}
+
+
+// Accessor
+
+namespace ers {
+    template<typename T>
+    class Accessor {
+    public:
+        // Constructors
+
+        Accessor(const IResource<T>& source) :
+            m_source(&source) {
+            _acquire();
+        }
+
+
+        Accessor(const Accessor& other) :
+            m_source(other.m_source) {
+            _acquire();
+        }
+        Accessor& operator=(const Accessor& other) {
+            if (this == &other)
+                return *this;
+
+            if (m_source)
+                _release();
+
+            m_source = other.m_source;
+            _acquire();
+
+            return *this;
+        }
+
+
+        Accessor(Accessor&& other) noexcept :
+            m_source(other.m_source) {
+            other.m_source = nullptr;
+        }
+        Accessor& operator=(Accessor&& other) noexcept {
+            if (m_source)
+                _release();
+            
+            m_source = other.m_source;
+            other.m_source = nullptr;
+            
+            return *this;
+        }
+
+
+        // Destructor
+
+        ~Accessor() {
+            _release();
+        }
+
+
+        // Accessors
+
+        T* operator->() { return _data(); }
+        const T* operator->() const { return _data(); }
+
+        T& operator*() { return *_data(); }
+        const T& operator*() const { return *_data(); }
+
+
+    protected:
+        const IResource<T>* m_source = nullptr;
+
+
+    private:
+        T* _data() {
+            ERS_ASSERT(m_source && m_source->m_value);
+            return m_source->m_value;
+        }
+        const T* _data() const {
+            ERS_ASSERT(m_source && m_source->m_value);
+            return m_source->m_value;
+        }
+
+
+        void _acquire() {
+            m_source->m_refs.fetch_add(1, std::memory_order_relaxed);
+        }
+
+        void _release() {
+            if (m_source->m_refs.fetch_sub(1, std::memory_order_release) == 1) {
+                std::atomic_thread_fence(std::memory_order_acquire);
+                boost::unique_lock lock(m_source->_mutex);
+                m_source->unload();
+            }
+
+            m_source = nullptr;
+        }
     };
 }
