1bb5c518c691ea5ea3ec2774ab806f6a7cb74313
EilRoviSoft <el.rovisoft@gmail.com>
2026-03-04 20:55:47 +0300

    Added custom deleter with callback for IResource

--- include/erslib/type/resource.hpp
+++ include/erslib/type/resource.hpp
@@ -21,15 +21,15 @@ namespace ers {
         // Destructor
 
         virtual ~IResource() = default;
-        
-        
+
+
         // Observers
 
         Result<storage_type> get() const {
-            boost::upgrade_lock read_lock(_mutex);
+            boost::upgrade_lock r_lock(_mutex);
 
             if (!m_value) {
-                boost::upgrade_to_unique_lock write_lock(read_lock);
+                boost::upgrade_to_unique_lock w_lock(r_lock);
 
                 if (auto s = load(); !s) {
                     m_value = nullptr;
@@ -46,10 +46,34 @@ namespace ers {
 
 
         virtual Status load() const = 0;
+        virtual Status unload() const {
+            m_value.reset();
+            return ok;
+        }
 
 
     private:
         mutable boost::upgrade_mutex _mutex;
+
+
+        storage_type _make_external_ptr() {
+            auto self = this;
+
+            return storage_type(
+                m_value.get(),
+                [self, inner = m_value](T*) mutable {
+                    inner.reset();
+
+                    boost::unique_lock r_lock(self->_mutex);
+
+                    if (self->m_value && self->m_value.use_count() == 1) {
+                        boost::upgrade_to_unique_lock w_lock(r_lock);
+
+                        self->unload();
+                    }
+                }
+            );
+        }
     };
 
 
