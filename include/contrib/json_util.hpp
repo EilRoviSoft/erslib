42c063eda2ea82d8844eddf58cb176585ab40914
Denis Kopein <el.rovisoft@gmail.com>
2026-05-06 13:36:58 +0300

    Contrib/JsonUtil: added

---
+++ include/contrib/json_util.hpp
@@ -0,0 +1,112 @@
+#pragma once
+
+// std
+#include <functional>
+#include <list>
+
+// utl
+#include <contrib/json.hpp>
+
+// ers
+#include <erslib/type/optional.hpp>
+#include <erslib/type/result.hpp>
+
+
+namespace ers::util {
+    class Schema {
+    public:
+        explicit Schema(const utl::Json& json) :
+            m_json(json) {
+        }
+
+
+        template<typename T>
+        void require(std::string_view name) {
+            const auto& object = m_json.as_object();
+
+            if (auto r = _check<T>(object, name); !r)
+                m_error = std::move(r.error());
+        }
+
+
+        template<typename T>
+        void write_if_exist(std::string_view name, T& out) {
+            const auto& object = m_json.as_object();
+
+            if (auto r = _check<T>(object, name); r) {
+                m_assignments.emplace_back([&out, value = r->second] {
+                    out = value.template as<T>();
+                });
+            }
+        }
+
+
+        template<typename T>
+        void require_and_write(std::string_view name, T& out) {
+            const auto& object = m_json.as_object();
+
+            if (auto r = _check<T>(object, name); r) {
+                m_assignments.emplace_back([&out, value = r->second] {
+                    out = value.template as<T>();
+                });
+            } else
+                m_error = std::move(r.error());
+        }
+
+        template<typename T>
+        void require_and_write(std::string_view name, T& out, std::function<T(std::string_view)> writer) {
+            const auto& object = m_json.as_object();
+
+            if (auto r = _check<T>(object, name); r) {
+                m_assignments.emplace_back([&out, writer = std::move(writer), value = r->second] {
+                    out = writer(value.template as<T>());
+                });
+            } else
+                m_error = std::move(r.error());
+        }
+
+
+        Status finalize() const {
+            if (m_error)
+                return Unexpected(*m_error);
+
+            for (const auto& assign : m_assignments)
+                assign();
+
+            return ok;
+        }
+
+
+    protected:
+        const utl::Json& m_json;
+        std::optional<Error> m_error;
+        std::list<std::function<void()>> m_assignments;
+
+
+    private:
+        template<typename T>
+        static Result<utl::Json::object_type::iterator> _check(utl::Json::object_type object, std::string_view name) {
+            auto it = object.find(name);
+
+            if (it == object.end()) {
+                return Unexpected<Error>(
+                    Severity::Warning,
+                    "parse_error",
+                    "Json doesn't have field with name \"{}\"",
+                    name
+                );
+            }
+
+            if (!it->second.is<T>()) {
+                return Unexpected<Error>(
+                    Severity::Warning,
+                    "parse_error",
+                    "Field with name \"{}\" has mismatched type \"{}\"",
+                    name, ers::meta::type_name_v<T>
+                );
+            }
+
+            return it;
+        }
+    };
+}
--- test/case/json.cpp
+++ test/case/json.cpp
@@ -3,10 +3,30 @@
 
 // contrib
 #include <contrib/json.hpp>
+#include <contrib/json_util.hpp>
 
 
 TEST_CASE("assignment", "[json]") {
     utl::Json obj;
 
     obj["hello"] = "world";
+}
+
+
+TEST_CASE("require_field", "[json]") {
+    utl::Json obj;
+
+    obj["int"] = 42;
+    obj["float"] = 3.14;
+    obj["str"] = "hello world";
+
+
+    std::string text;
+
+
+    auto s = ers::util::Schema(obj)
+        .require<int64_t>("int")
+        .require<double_t>("float")
+        .require_and_write("str", text)
+        .finalize();
 }
